5faed3e8efd2a10935062b65a475679396a1f722
zaculizate1996 <alancabot.ac@gmail.com>
2020-03-17 19:39:02 -0300

    Se agrega ejercicio operadores de asignacion y comparacion

---
+++ OPERADORES DE ASIGNACIÓN Y COMPARACIÓN/script.js
@@ -0,0 +1,46 @@
+/* 
+
+Example 1 (Operadores de Asignación):
+
+    let edad = 38
+    undefined
+
+    edad += 1
+    39
+
+    edad = edad + 1
+    40
+
+    edad -= 1
+    39
+
+    edad *= 2
+    78
+
+    edad /= 3 
+    26
+
+Example 2 (Operadores de Comparación):
+
+    5 == '5'
+    true
+
+    5 === '5'
+    false
+
+    5 != '5'
+    false
+
+    5 !== '5' !important
+    true
+
+    'a' > 'b'
+    false
+
+    'b' > 'a'
+    true
+    
+*/
+
+
+
