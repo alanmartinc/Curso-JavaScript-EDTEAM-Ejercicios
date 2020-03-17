7bc45490c25769866ba50c840d1938ded00af7e5
zaculizate1996 <alancabot.ac@gmail.com>
2020-03-17 20:49:14 -0300

    Se agrega ejercicio recorrer arrays con for y for of

---
+++ RECORRER ARRAYS CON FOR Y FOR OF/script.js
@@ -0,0 +1,26 @@
+// Recorrer arrays con for y for of
+
+let arr = ['a','b','c','d','e','f']
+undefined
+
+for(let i=0; i < arr.length; i++) { 
+    console.log(arr[i])
+}
+
+a
+b
+c
+d
+e
+f
+
+for(let element of arr) { 
+    console.log(element) 
+}
+
+a
+b
+c
+d
+e
+f
