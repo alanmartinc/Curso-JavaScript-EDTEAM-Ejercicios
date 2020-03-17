647cb08d3f43cce4c8f7280ee747c60e88249a97
zaculizate1996 <alancabot.ac@gmail.com>
2020-03-17 20:10:37 -0300

    Se agrega ejercicio while

---
+++ WHILE/script.js
@@ -0,0 +1,15 @@
+// While
+
+/*
+let i = 10
+while(i = 10) {
+    console.log(i)
+    i--
+}
+*/
+
+let password = 'EDteam'
+let pass
+do {
+    pass = prompt('Ingrese la contraseña')
+} while(pass !== password)
