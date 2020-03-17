610279a0b9359c2c1da598f010a87bdcbf62d8f6
zaculizate1996 <alancabot.ac@gmail.com>
2020-03-17 20:33:45 -0300

    Se agrega ejercicio desestructuracion de arrays

---
+++ DESESTRUCTURACIÓN DE ARRAYS/script.js
@@ -0,0 +1,37 @@
+// Desestructuración de Arrays
+
+let arr5 = ['Hola', 'amigos', 'de', 'EDteam']
+undefined
+
+arr5
+(4) ["Hola", "amigos", "de", "EDteam"]
+
+arr5[3]
+"EDteam"
+
+let string1 = arr5[0]
+undefined
+
+let string2 = arr5[1]
+undefined
+
+string1
+"Hola"
+
+string2
+"amigos"
+
+let [s1,s2,s3,s4] = arr5
+undefined
+
+s1
+"Hola"
+
+s2
+"amigos"
+
+s3
+"de"
+
+s4
+"EDteam"
