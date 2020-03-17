5d20657eb83d6dd740f2d9f506328be6e3f45338
zaculizate1996 <alancabot.ac@gmail.com>
2020-03-17 20:40:46 -0300

    Se agrega ejercicio unir arrays, convertir arrays a strings

---
+++ UNIR ARRAYS, CONVERTIR ARRAYS A STRINGS/script.js
@@ -0,0 +1,29 @@
+// Unir arrays, convertir arrays a string
+
+let numbers = [10, 100, 2, 20]
+undefined
+
+numbers.join(' ')
+"10 100 2 20"
+
+numbers.join(',')
+"10,100,2,20"
+
+let number2 = [99,999,9999]
+undefined
+
+numbers.concat(number2)
+(7) [10, 100, 2, 20, 99, 999, 9999]
+
+numbers.push(10)
+5
+
+numbers
+(5) [10, 100, 2, 20, 10]
+
+numbers.concat(7,17,27)
+(8) [10, 100, 2, 20, 10, 7, 17, 27]
+
+number2
+(3) [99, 999, 9999]
+
