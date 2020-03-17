431cddb417ca7d8d39b85f5011a47631d9aca85d
zaculizate1996 <alancabot.ac@gmail.com>
2020-03-17 19:50:49 -0300

    Se agrega ejercicio numeros

---
+++ NUMEROS/script.js
@@ -0,0 +1,59 @@
+/* 
+
+Example 1 ToFixed(): Especifica el número de decimales: number.toFixed(n)
+
+    number.toFixed(2)
+    "5.00"
+
+    let number2 = 9078.5489164
+    undefined
+
+    number2
+    9078.5489164
+
+    number2.toFixed(3)
+    "9078.548"
+
+Example 2 ParseInt() y ParseFloat(): Convierte el texto a numero
+
+    let texto = '20'
+    undefined
+    
+    texto
+    "20"
+
+    let textToNumber = parseInt(texto,10)
+    undefined
+
+    textToNumber
+    20
+
+    parseFloat(texto)
+    20
+
+    texto = '20.12'
+    "20.12"
+
+    parseInt(texto,10)
+    20
+
+    parseInt(texto)
+    20.12
+
+    Math.ceil(20.1)
+    21
+
+    Math.round(20.1)
+    20
+
+    Math.round(20.6)
+    21
+
+    Math.random() = Numero aleatoreo
+
+    Math.ceil(Math.ramdom() * 10) = Numero al azar entre 1 y 10
+    8
+    
+*/
+
+
