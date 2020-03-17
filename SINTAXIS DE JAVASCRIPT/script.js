7e8bdd52b4035cb72ddbe1267ed9207ed5100b10
zaculizate1996 <alancabot.ac@gmail.com>
2020-03-17 19:25:18 -0300

    Se agrega ejercicio sintaxis de javascript

---
+++ SINTAXIS DE JAVASCRIPT/script.js
@@ -0,0 +1,25 @@
+let numero = 18
+
+console.log(numero)
+console.log(numero + 1)
+console.log(numero * 2)
+console.log(numero / 3)
+
+if(19 > numero) {
+    console.log(numero)
+}
+
+/*
+    Sintaxis:
+
+    *Comentarios
+    *Punto y coma
+    *Espacios en blanco
+    *Case sensitive
+    *Expresiones
+*/
+
+const number = 20; const edad = 50
+
+let UserAge = 23 // CalmelCase
+
