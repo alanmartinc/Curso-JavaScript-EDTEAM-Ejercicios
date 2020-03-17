c028996352eeb9f8192485a9c9fd2f3006734bc5
zaculizate1996 <alancabot.ac@gmail.com>
2020-03-17 19:48:14 -0300

    Se agrega ejercicio operador de corto circuito

---
+++ OPERADOR DE CORTO CIRCUITO/script.js
@@ -0,0 +1,37 @@
+/* 
+
+Example 1 (Operador de Corto Circuito):
+
+    let a
+    undefined
+
+    let b = a || 'Alan'
+    undefined
+
+    b
+    "Alan"
+
+    let c = null || 25
+    undefined
+
+    c
+    25
+
+    led d = null || a
+    undefined
+
+    let e = null && a
+    undefined
+
+    e
+    null
+
+    let f = 'hola' && 'mundo'
+    undefined 
+
+    f
+    "mundo"
+    
+*/
+
+
