a4e5b7c9e3f1493ac3088155b712b65eee4e1f60
zaculizate1996 <alancabot.ac@gmail.com>
2020-03-17 19:54:47 -0300

    Se agrega ejercicio encontrar cadenas de texto

---
+++ ENCONTRAR CADENAS DE TEXTO/script.js
@@ -0,0 +1,46 @@
+/* 
+
+Example 1 (Encontrar Cadenas de Texto)
+
+    *indexOf(string[,i]) = Encontrar caracteres
+
+    let profesor = 'Alan'
+    undefined
+
+    alan.indexOf('r')
+    -1
+
+    'Hola amigos'.indexOf('o')
+    1
+
+    'Hola amigos'.indexOf('o', 2)
+    9
+
+    'Hola amigos'.indexOf('o', 'Hola amigos'.indexOf('o') + 1)
+    9
+
+    *lastIndexOf() = Encontrar caracteres desde atras
+
+    'Hola amigos'.lastIndexOf('o')
+    9
+
+    *includes(string[,i]) = Pregunta si el texto incluye cierta cadena de texto
+
+    'https://ed.team/blog'.include('blog')
+    true
+
+    *startsWith(string[,i]) = Pregunta si comienza con determinado texto
+
+    'https://ed.team/blog'.startsWith('https://ed.team')
+    true
+
+    *endsWith(string[,i]) = Pregunta como termina un texto
+
+    'https://ed.team/blog'.endsWith('https://ed.team')
+    false
+
+*/
+
+
+
+
