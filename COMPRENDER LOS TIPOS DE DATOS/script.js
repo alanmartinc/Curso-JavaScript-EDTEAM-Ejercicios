174308d2e19ac103a52423e5faf2e5fda1ccbf27
zaculizate1996 <alancabot.ac@gmail.com>
2020-03-17 19:30:09 -0300

    Se agrega ejercicio comprender los tipos de datos

---
+++ COMPRENDER LOS TIPOS DE DATOS/script.js
@@ -0,0 +1,28 @@
+/* 
+    typeof 5 = "number"
+    typeof hola = "string"
+    typeof true = "boolean"
+    typeof Symbol() = "symbol"
+    typeof undefined = "undefined"
+    typeof null = "object" (Error)
+    typeof {} = "object"
+    typeof [] = "object"
+    typeof function(){} = "function"
+
+    Dinamicamente tipado:
+
+    let myVar = 10
+    undefined
+
+    typeof myVar
+    "number"
+
+    myVar = "Hola Mundo"
+    "Hola Mundo"
+
+    typeof myVar
+    "string"
+*/
+
+
+
