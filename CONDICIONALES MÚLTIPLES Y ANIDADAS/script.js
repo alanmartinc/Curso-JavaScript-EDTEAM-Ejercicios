0028bb1f1d836ddda5b7ea74d234c83b8de14d3d
zaculizate1996 <alancabot.ac@gmail.com>
2020-03-17 20:00:41 -0300

    Se agrega ejercicio condiciones multiples y anidadas

---
+++ CONDICIONALES MÚLTIPLES Y ANIDADAS/script.js
@@ -0,0 +1,16 @@
+// Condicionales
+
+if(3 > 2 && 5 > 2) {
+    console.log('Pasó la condición 1')
+} else if (2 < 1 || 2 > 1){
+    console.log('Pasó la condición 2')
+}
+
+// Condicion Anidada
+
+if (3 > 2) {
+    console.log('Pasó la condición 1.1')
+    if (5 > 7) {
+        console.log('Pasó la condición 1.2')
+    }
+}
