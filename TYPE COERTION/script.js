a019e69801b6d189bc719497758671d5ee0ae684
zaculizate1996 <alancabot.ac@gmail.com>
2020-03-17 19:32:12 -0300

    Se agrega ejercicio type coertion

---
+++ TYPE COERTION/script.js
@@ -0,0 +1,34 @@
+/* 
+
+Example 1:
+
+    'Hola' + ' ' + 'mundo'
+    "Hola mundo"
+
+    let user = 'Alan'
+    undefined
+
+    console.log('Hola ' + user)
+    "Hola Alan"
+
+Example 2:
+
+    'Hola' * 10
+    NaN
+
+    '2' * 20
+    40
+
+    true + 1
+    2
+
+    false + 2
+    2
+
+    false == ''
+    true
+    
+*/
+
+
+
