4268e4ff51e61c343361a113d3e0fd2b98c0e151
zaculizate1996 <alancabot.ac@gmail.com>
2020-03-17 19:27:49 -0300

    Se agrega ejercicio tipos de datos

---
+++ TIPOS DE DATOS/script.js
@@ -0,0 +1,31 @@
+// String: `` "" ''
+// Boolean: True or False
+// Indefined: No está definida el valor
+// Null: No existe
+// Symbol(): Crea referencias unicas 
+// Typeof: Devuelve el tipo de dato como string
+
+let userage = 23
+let nombre = `Alan tiene ${userage} años`
+
+let div = `
+    <div class="container">
+        ${nombre}
+    </div>
+`
+
+document.body.innerHTML = div
+
+console.log(nombre)
+
+/* Array: let numbers = [1,2,3,4,5,6,7] */
+
+/* Object: let data = {
+    nombre: "Alan",
+    apellido: "...",
+    edad: 23
+} */
+
+
+
+
