50a0bfa6d9f7693d82d03f6833cbcabbb9982ce1
zaculizate1996 <alancabot.ac@gmail.com>
2020-03-17 19:45:15 -0300

    Se agrega ejercicio operadores ternario

---
+++ OPERADORES TERNARIO/script.js
@@ -0,0 +1,17 @@
+let age = prompt('Dime tu edad')
+
+let isAdult = age >= 18
+                ? 'Eres mayor de edad'
+                : 'Aun eres un niño'
+
+                /*
+                    Expresión que devuelve Boolean
+                    ? valor si es true
+                    : valor si es false
+                */
+alert(isAdult)
+
+
+
+
+
