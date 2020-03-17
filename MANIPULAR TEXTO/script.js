c5aeb55c04ed2f65dd771e78b03891ac7fd51dfa
zaculizate1996 <alancabot.ac@gmail.com>
2020-03-17 19:57:01 -0300

    Se agrega ejercicio manipular texto

---
+++ MANIPULAR TEXTO/script.js
@@ -0,0 +1,61 @@
+/* 
+
+Example 1 (Manipular Texto)
+
+    *replace(original,replacement) = Toma un texto y lo reemplaza por otro
+
+    'Hola mundo'.replace('mundo','Alan')
+    "Hola alan"
+
+    *split(separator[,quantity]) = Separar un texto
+
+    'Alan'.split('n')
+    (2) ["Ala", ""]
+
+    'Hola mundo'.split(' ')[1]
+    "mundo"
+
+    'Alan'.split('')
+    (4) ["A", "l", "a", "n"]
+
+    'Alan'.split('')[2]
+    "a"
+
+    'Alan'[2]
+    "a"
+
+    *substring(start[,end]) = Extrae el texto - Incluye and pero no a end
+
+    'Hola mundo'.substring(4)
+    " mundo"
+
+    'Hola mundo'.substring(2)
+    "la mundo"
+
+    'Hola mundo'.substring(2,5)
+    "la "
+
+    'Hola mundo'.substring(2,-1)
+    "Ho"
+
+    *substr(start[,quantity]) = Cantidad de caracteres
+
+    'Hola mundo'.substr(2,5)
+    "la mu"
+
+    'Hola mundo'.substr(-4)
+    "undo"
+
+    *slice(start[,end]) = 
+
+    'Hola mundo'.slice(-5,-2)
+    "mun"
+
+    'Hola mundo'.slice(1,-1)
+    "ola mund"
+
+*/
+
+
+
+
