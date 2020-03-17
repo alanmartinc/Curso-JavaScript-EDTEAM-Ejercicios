f703f94822857d9293e4c1bfc9928f5e251e38b3
zaculizate1996 <alancabot.ac@gmail.com>
2020-03-17 19:36:25 -0300

    Se agrega ejercicio asignacion por valor o referencia

---
+++ ASIGNACIÓN POR VALOR O REFERENCIA/script.js
@@ -0,0 +1,60 @@
+/* 
+
+Example 1 (Tipos de Datos Primitivos):
+
+    let number = 50
+    undefined
+
+    let number =2 = number
+    undefined
+
+    number2
+    50
+
+    number2 = 60
+    60
+
+    number
+    50
+
+Example 2 (Tipos de Datos Primitivos):
+
+    let user1 = 'Alan'
+    undefined
+
+    let user2 = user1
+    undefined
+
+    user2
+    "Alan"
+
+    user2 = 'Martin'
+    "Martin"
+
+    user1
+    "Alan"
+
+Example 3 Arrays (Tipos de Datos Compuestos):
+
+    let numbers1 = [1,2,3,4]
+    undefined
+
+    let numbers2 = numbers1
+    undefined
+
+    numbers2
+    (4) [1, 2, 3, 4]
+
+    numbers2.push(5)
+    5
+
+    numbers2
+    (5) [1, 2, 3, 4, 5]
+
+    numbers1
+    (5) [1, 2, 3, 4, 5]
+    
+*/
+
+
+
