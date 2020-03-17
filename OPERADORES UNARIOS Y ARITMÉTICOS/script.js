ecc07ca9b5c2a115e34e73830d8ff7520b4e8f7e
zaculizate1996 <alancabot.ac@gmail.com>
2020-03-17 19:42:35 -0300

    Se agrega ejercicio operadores unarios y aritmeticos

---
+++ OPERADORES UNARIOS Y ARITMÉTICOS/script.js
@@ -0,0 +1,64 @@
+/* 
+
+Example 1 (Operadores Unarios):
+
+    typeof 5
+    "number"
+
+    !true
+    false
+
+    !1
+    false
+
+    !0
+    true
+
+Example 2 (Operadores Aritméticos):
+
+    5 / 5
+    1
+
+    5 / 3
+    1.6666666666666667
+
+    5 % 3
+    2
+
+    10 % 2 === 0
+    true
+
+    13 % 2 === 0 
+    false
+
+    let number1 = 12
+    undefined
+
+    number1--
+    12
+
+    number1
+    11
+
+    number1--
+    11
+
+    number1
+    10
+    
+    --number1
+    9
+
+    number1++
+    9
+
+    number1
+    10
+
+    ++number1
+    11
+    
+*/
+
+
+
