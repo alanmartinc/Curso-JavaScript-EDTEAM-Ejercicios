f892c9698b5218a7b79da86495d18101bb6c8733
zaculizate1996 <alancabot.ac@gmail.com>
2020-03-17 19:58:46 -0300

    Se agrega ejercicio condicionales y ciclos

---
+++ CONDICIONALES Y CICLOS/script.js
@@ -0,0 +1,23 @@
+// IF en una línea
+
+if ('a' === 'b') console.log('No se ejecuté')
+if (5 > 3 ) console.log('Me ejecuté')
+
+// IF en un bloque
+
+{
+    let empresa = 'EDteam'
+    console.log(empresa)
+} 
+
+if('a' === 'a') {
+    console.log('Me ejecuté nuevamente')
+}
+
+// IF con más condiciones
+
+if('a' === 'b') {
+    console.log('Pasó la condición')
+} else {
+    console.log('No pasó la condición')
+}
