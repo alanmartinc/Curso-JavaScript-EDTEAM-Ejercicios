c196f8e63f8c1d23df9c0905980f94113f111cd6
zaculizate1996 <alancabot.ac@gmail.com>
2020-03-17 22:50:10 -0300

    Se agrega ejercicio propiedades de los objetos

---
+++ PROPIEDADES DE LOS OBJETOS/script.js
@@ -0,0 +1,31 @@
+// Propiedades de los objetos
+
+let user = { nombre: 'Jorge' }
+undefined
+
+user
+{nombre: "Jorge"}
+
+user['nombre']
+"Jorge"
+
+let a = 'hola', b = 'mundo'
+undefined
+
+let saludo = { [a + b]: 'Mi primer hola mundo' }
+undefined
+
+saludo
+{holamundo: "Mi primer hola mundo"}
+
+let myObject = { a: a, b: b }
+undefined
+
+myObject
+{a: "hola", b: "mundo"}
+
+let myObject2 = {a,b}
+undefined
+
+myObject2
+{a: "hola", b: "mundo"}
