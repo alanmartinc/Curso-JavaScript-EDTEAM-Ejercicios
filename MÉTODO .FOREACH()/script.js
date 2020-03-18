289c2800cd774995dbad7b4703a87e21bf6d069c
zaculizate1996 <alancabot.ac@gmail.com>
2020-03-17 22:42:57 -0300

    Se agrega ejercicio metodo .forEach()

---
+++ MÉTODO .FOREACH()/script.js
@@ -0,0 +1,15 @@
+// Método .forEach()
+
+let numbers = [5,6,9,4,3]
+undefined
+
+let arr2 = []
+undefined
+
+numbers.forEach(el => { 
+    arr2.push(el*el)
+})
+undefined
+
+arr2
+(5) [25, 36, 81, 16, 9]
