c37fdd1d2c8f8d3a52d1ca9f215d72868c2b25a5
zaculizate1996 <alancabot.ac@gmail.com>
2020-03-17 23:00:57 -0300

    Se agrega ejercicio recorrer objetos

---
+++ RECORRER OBJETOS/script.js
@@ -0,0 +1,13 @@
+// Recorrer objetos
+
+user
+{nombre: "Jorge", edad: "40", sexo: "Masculino", piernas: 3}
+
+Object.entries(user)
+(4) [Array(2), Array(2), Array(2), Array(2)]
+
+Object.keys(user)
+(4) ["nombre", "edad", "sexo", "piernas"]0: "nombre"1: "edad"2: "sexo"3: "piernas"length: 4__proto__: Array(0)
+
+Object.values(user)
+(4) ["Jorge", "40", "Masculino", 3]
